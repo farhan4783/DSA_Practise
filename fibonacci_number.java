91d48fb2da8d277579c2faa74acbbcd90dd34070
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-03-28 16:56:17 +0530

    Implement bottom-up Fibonacci solution
    Added a bottom-up dynamic programming approach to calculate Fibonacci numbers.

--- fibonacci_number.java
+++ fibonacci_number.java
@@ -12,3 +12,23 @@ class Solution {
         return dp[n];
     }
 }
+
+
+//bottom up 
+
+
+class Solution {
+    public int fib(int n) {
+        if (n <= 1) return n;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
