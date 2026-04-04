cd6950003a57a4b95e41e7520c42257b761b1079
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-04-04 22:12:14 +0530

    Implement backtracking to generate parentheses

--- generate_parenthesis.java
+++ generate_parenthesis.java
@@ -16,3 +16,32 @@ class Solution {
     }
     
 }
+
+
+//using backtracking
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        ArrayList<String> res= new ArrayList<>();
+        solve(n, 0,0,new StringBuilder(),res);
+        return res;
+    }
+    void solve(int n, int l, int r, StringBuilder s, List<String> res){
+        if(s.length()==2*n){
+            res.add(s.toString());
+            return;
+        }
+        if(l<n){
+            s.append('(');
+            solve(n, l+1, r, s, res);
+            s.deleteCharAt(s.length()-1);
+        }
+        
+        if(r<l){
+            s.append(')');
+            solve(n, l, r+1, s, res);
+            s.deleteCharAt(s.length()-1);
+        }
+
+    }
+}
+
