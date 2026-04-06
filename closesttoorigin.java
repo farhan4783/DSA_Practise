12ce1a67dd5ca16dd3f64e2a89076f60e40c6280
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-04-06 14:18:49 +0530

    Implement kClosest method using quick select

--- closesttoorigin.java
+++ closesttoorigin.java
@@ -32,3 +32,58 @@ class Solution {
     }
 }
 
+
+// this
+
+
+
+import java.util.*;
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        quickSelect(points, 0, points.length - 1, k);
+        return Arrays.copyOfRange(points, 0, k);
+    }
+
+    private void quickSelect(int[][] points, int left, int right, int k) {
+
+        if (left >= right) return;
+
+        int pivotIndex = partition(points, left, right);
+
+        if (pivotIndex == k) return;
+        else if (pivotIndex < k) {
+            quickSelect(points, pivotIndex + 1, right, k);
+        } else {
+            quickSelect(points, left, pivotIndex - 1, k);
+        }
+    }
+
+    private int partition(int[][] points, int left, int right) {
+
+        int[] pivot = points[right];
+        int pivotDist = dist(pivot);
+
+        int i = left;
+
+        for (int j = left; j < right; j++) {
+            if (dist(points[j]) <= pivotDist) {
+                swap(points, i, j);
+                i++;
+            }
+        }
+
+        swap(points, i, right);
+        return i;
+    }
+
+    private int dist(int[] p) {
+        return p[0]*p[0] + p[1]*p[1];
+    }
+
+    private void swap(int[][] points, int i, int j) {
+        int[] temp = points[i];
+        points[i] = points[j];
+        points[j] = temp;
+    }
+}
