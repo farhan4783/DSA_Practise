845cf0c3b8ca80929123e5be8f1ab782a76a4c1f
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-04-08 13:06:55 +0530

    Add solution for Assign Cookies problem
    Implement a solution to find the maximum number of content children using a greedy algorithm.

---
+++ Assign_cookies.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+    
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int childPointer = 0;
+        int cookiePointer = 0;
+        
+       
+        while (childPointer < g.length && cookiePointer < s.length) {
+            
+
+
+            if (s[cookiePointer] >= g[childPointer]) {
+
+                childPointer++; 
+                
+                
+                
+                
+             }
+           
+
+           
+            cookiePointer++;
+        }
+        
+        return childPointer;
+    }
+}
