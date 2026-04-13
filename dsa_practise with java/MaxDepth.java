17183384a731da4784cd1a3311db1f16225435d7
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-04-13 10:49:49 +0530

    Implement maxDepth method for binary tree

--- dsa_practise with java/MaxDepth.java
+++ dsa_practise with java/MaxDepth.java
@@ -43,3 +43,19 @@ class Solution {
 
     }
 }
+
+
+//another
+
+class Solution {
+    int max=0;
+    public int maxDepth(TreeNode root) {
+
+    if (root ==null){
+        return 0;
+    }
+    int leftDepth = maxDepth(root.left);
+    int rightDepth = maxDepth(root.right);
+    return 1 + Math.max(leftDepth,rightDepth);
+    }
+}
