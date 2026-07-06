e089cce10f2438a867bd7b83b4e215c4b22b526e
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-07-06 11:31:47 +0530

    Add solution for removing covered intervals

---
+++ dsa_practise with java/covered intervals.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            }
+            return a[0] - b[0];
+        });
+
+        int remainingCount = 0;
+        int currentRightBound = 0;
+
+        for (int[] interval : intervals) {
+            if (interval[1] > currentRightBound) {
+                remainingCount++;
+                currentRightBound = interval[1];
+            }
+        }
+
+        return remainingCount;
+    }
+}
