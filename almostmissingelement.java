910f34d26ec41b4a8e764fa44b71fed8a035095e
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-08-18 20:24:05 +0530

    Add optimal solution for largest integer problem
    Implemented an optimal approach to find the largest integer in an array based on frequency and conditions.

--- almostmissingelement.java
+++ almostmissingelement.java
@@ -27,3 +27,56 @@ class Solution {
         return maxAlmostMissing;
     }
 }
+
+
+
+
+
+/////////////////////////////////////////////   optimal approach according to me ///////////////////////////////////////////////////////////
+
+
+
+import java.util.*;
+
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        int n = nums.length;
+        
+      
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+      
+        if (k == 1) {
+            int ans = -1;
+            for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+                if (entry.getValue() == 1) {
+                    ans = Math.max(ans, entry.getKey());
+                }
+            }
+            return ans;
+        }
+
+     
+        if (k == n) {
+            int maxVal = -1;
+            for (int num : nums) {
+                maxVal = Math.max(maxVal, num);
+            }
+            return maxVal;
+        }
+
+      
+        int ans = -1;
+        if (freq.get(nums[0]) == 1) {
+            ans = Math.max(ans, nums[0]);
+        }
+        if (freq.get(nums[n - 1]) == 1) {
+            ans = Math.max(ans, nums[n - 1]);
+        }
+
+        return ans;
+    }
+}
