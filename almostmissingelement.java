759f956f704fc01f66ac27077a81be4e7825a784
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-08-18 20:20:29 +0530

    Implement largestInteger method to find unique max

---
+++ almostmissingelement.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer> subarrayCount = new HashMap<>();
+
+       
+        for (int i = 0; i <= n - k; i++) {
+            Set<Integer> uniqueInSubarray = new HashSet<>();
+            for (int j = i; j < i + k; j++) {
+                uniqueInSubarray.add(nums[j]);
+            }
+           
+            for (int num : uniqueInSubarray) {
+                subarrayCount.put(num, subarrayCount.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        int maxAlmostMissing = -1;
+        for (Map.Entry<Integer, Integer> entry : subarrayCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                maxAlmostMissing = Math.max(maxAlmostMissing, entry.getKey());
+            }
+        }
+
+        return maxAlmostMissing;
+    }
+}
